5bf2009f2f182bf463c097014cce12db4d31231a
isagomz <isaiasgomz027@gmail.com>
2022-06-21 12:12:13 -0500

    homework 11

--- FT-M2-master/11-Redux/hw-js/src/actions/index.js
+++ FT-M2-master/11-Redux/hw-js/src/actions/index.js
@@ -1,15 +1,29 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const { INCREMENTO, DECREMENTO , INCREMENTO_IMPAR, INCREMENTO_ASYNC } = require('../action-types');
 
 // Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá. 
 // ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable 
 // de manejar ninguna de las lógicas actuales de actualización del store central de Redux.
 // Eso se lo deja al reducer(s).
 
-const incremento = null;
+const incremento =function () {
+  {type: INCREMENTO}
+}
 
-const decremento = null;
+const decremento = function()  {
+  {type: DECREMENTO}
+}
+
+const incrementoImpar = function()  {
+  {type: INCREMENTO_IMPAR}
+}
+
+const incrementoAsync = function() { 
+  {type: INCREMENTO_ASYNC}
+}
 
 module.exports = {
   incremento,
-  decremento
+  decremento,
+  incrementoImpar,
+  incrementoAsync
 }
